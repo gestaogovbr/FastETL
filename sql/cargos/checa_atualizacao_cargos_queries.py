d7ad33a7edfaa2623e87094c3626746b431282ef
Vitor Bellini <vitor.bellini@planejamento.gov.br>
2019-07-22 19:17:16 -0300

    testa consulta monitoramento

--- sql/cargos/checa_atualizacao_cargos_queries.py
+++ sql/cargos/checa_atualizacao_cargos_queries.py
@@ -1,6 +1,11 @@
-query_checa_atualizacao = """
+query_checa_atualizacao_2 = """
     SELECT COUNT(1)
     FROM PGG_DW.DW_APF_FATOS.FT_INFORMACOES_SIAPE
     WHERE INFORMACAO_TIPO_ID = 20001
         AND SUBSTRING(CONVERT(VARCHAR, TEMPO_DIA_ID), 1, 6) = '{{ macros.ds_format(macros.ds_add(ds, -25), "%Y-%m-%d", "%Y%m") }}';
+"""
+query_checa_atualizacao = """
+    SELECT *
+FROM PGG_DW.CONTROLE.sensor_test
+WHERE ID = '{{ macros.ds_format(macros.ds_add(ds, -25), "%Y-%m-%d", "%Y%m") }}';
 """
