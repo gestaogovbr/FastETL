26d19ed5f1b30263901c64eb840443d4a6abce87
Vitor Bellini <vitor.bellini@planejamento.gov.br>
2019-07-29 15:36:40 -0300

    testa retry_callback e check_operator

--- sql/cargos/cargos_queries.py
+++ sql/cargos/cargos_queries.py
@@ -78,5 +78,5 @@ query_checa_atualizacao = """
 SELECT COUNT(1) as qt_registros
 FROM PGG_DW.DW_APF_FATOS.FT_INFORMACOES_SIAPE
 WHERE INFORMACAO_TIPO_ID = 20001
-	AND SUBSTRING(CONVERT(VARCHAR, TEMPO_DIA_ID), 1, 6) = 201907;
+	AND SUBSTRING(CONVERT(VARCHAR, TEMPO_DIA_ID), 1, 6) = {{ macros.ds_format(macros.ds_add(ds, -25), "%Y-%m-%d", "%Y%m") }};
 """
