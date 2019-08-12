b1962505c3dc32ae5efe03995ed1d3af60ed184b
Vitor Bellini <vitor.bellini@planejamento.gov.br>
2019-08-12 15:55:39 -0300

    adiciona test sql monitora pessoal

--- sql/pessoal/checa_atualizacao_pessoal.sql
+++ sql/pessoal/checa_atualizacao_pessoal.sql
@@ -1,7 +1,7 @@
 -- código definitivo
 SELECT COUNT(1)
 FROM PGG_DW.DW_APF_FATOS.FT_INFORMACOES_SIAPE
-WHERE INFORMACAO_TIPO_ID = 30001 AND SUBSTRING(CONVERT(VARCHAR, TEMPO_DIA_ID), 1, 6) = {{ macros.datetime(execution_date.year, execution_date.month -1, 1).strftime("%Y%m") }};
+WHERE INFORMACAO_TIPO_ID = %s AND SUBSTRING(CONVERT(VARCHAR, TEMPO_DIA_ID), 1, 6) = {{ macros.datetime(execution_date.year, execution_date.month -1, 1).strftime("%Y%m") }};
 
 -- SELECT COUNT(1)
 -- FROM PGG_DW.DW_APF_FATOS.FT_INFORMACOES_SIAPE
