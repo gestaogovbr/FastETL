f8b54525bdcad8d70ffb1578c0a5d692bd50fcce
Vitor Bellini <vitor.bellini@planejamento.gov.br>
2019-08-15 17:28:22 -0300

    fix typo

--- sql/cargos/atualiza_backup_fato.sql
+++ sql/cargos/atualiza_backup_fato.sql
@@ -50,4 +50,4 @@ SELECT
 	, UORG_VINC
 	, FT_INFORMACOES_SIAPE_DT_INCLUSAO
 FROM PGG_DW.DW_APF_FATOS.FT_INFORMACOES_SIAPE
-WHERE INFORMACAO_TIPO_ID = 20001 AND SUBSTRING(CONVERT(VARCHAR, TEMPO_DIA_ID), 1, 6) <> {{ macros.datetime(execution_date.year, execution_date.month -1, 1).strftime("%Y%m") }};
+WHERE INFORMACAO_TIPO_ID = 20001 AND SUBSTRING(CONVERT(VARCHAR, TEMPO_DIA_ID), 1, 6) = {{ macros.datetime(execution_date.year, execution_date.month -1, 1).strftime("%Y%m") }};
