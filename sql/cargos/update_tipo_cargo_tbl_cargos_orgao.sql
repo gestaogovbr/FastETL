4e1562c7387602d60310939aa78a44c5e2276e69
Vitor Bellini <vitor.bellini@planejamento.gov.br>
2019-08-30 10:06:28 -0300

    altera estrutura de pastas

--- sql/cargos/update_tipo_cargo_tbl_cargos_orgao.sql
+++
@@ -1,8 +0,0 @@
-UPDATE PGG_DW.CONTROLE.cargos_orgao
-SET cargo_tipo = (CASE
-					WHEN SUBSTRING(cargo_descricao, 1, 3) like '[Aa-Zz][Aa-Zz][Aa-Zz]'
-						THEN SUBSTRING(cargo_descricao, 1, 3)
-					ELSE
-						SUBSTRING(cargo_descricao, 1, 2)
-					END)
-FROM PGG_DW.CONTROLE.cargos_orgao
