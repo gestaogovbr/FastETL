ac13537661714516da9b8852713dd928a1987ab9
Nitai Bezerra da Silva <nitai.silva@planejamento.gov.br>
2020-11-25 10:47:16 -0300

    feat(bacen_sta_hook): lots of improvement
    Add max_date to range filter. Add exception to wrong range date.

--- plugins/hooks/bacen_STA_hook.py
+++ plugins/hooks/bacen_STA_hook.py
@@ -3,6 +3,7 @@ Hook para utilização do STA (API do Bancon Central - Bacen). Geralmente
 utilizada para download de arquivos de dados.
 """
 from datetime import datetime, timedelta
+import pytz
 import xml.etree.ElementTree as ET
 import requests
 
@@ -32,23 +33,32 @@ class BacenSTAHook(BaseHook):
             }
         return headers
 
-    def _get_id_newest_file(self, lastdays_filter: int) -> str:
+    def _get_id_newest_file(self, data_min, data_max=None) -> str:
         """
-        Filtra os arquivos do STA pela data e retorna o id (protocolo) do
-        arquivo mais recente.
+        Filtra os arquivos do STA pelo intervalo de datas e retorna o id
+        (protocolo) do arquivo mais recente.
         """
-        data_inicio = (datetime.now() - timedelta(days=lastdays_filter))
+
+        tz = pytz.timezone("America/Sao_Paulo")
+        if data_max is None:
+            data_max = datetime.now(tz)
+        elif data_max > datetime.now(tz):
+            raise Exception('data_max não pode ser maior que data atual.' \
+                            ' É necessário considerar o timezone do Airflow.')
+
         querystring = {
-            "dataHoraInicio": data_inicio.strftime(self.DATE_FORMAT)[:23],
+            "dataHoraInicio": data_min.strftime(self.DATE_FORMAT)[:23],
+            "dataHoraFim": data_max.strftime(self.DATE_FORMAT)[:23],
             "sistemas": self.sistema
             }
         url = self.STA_URL + "/arquivos/disponiveis"
-        list_xml = requests.request("GET",
+        response = requests.request("GET",
                                     url,
                                     headers=self._get_request_headers(),
                                     params=querystring)
+        # TODO: Tratar erro de login/senha com mensagem esclarecedora
 
-        xml_tree = ET.fromstring(list_xml.content)
+        xml_tree = ET.fromstring(response.content)
 
         extract_date = lambda x: datetime.strptime(x, self.DATE_FORMAT)
         data_id_map = {
@@ -61,12 +71,13 @@ class BacenSTAHook(BaseHook):
 
     def download_latest_file(self,
                              dest_file_path: str,
-                             lastdays_filter: int=30):
+                              data_min,
+                              data_max=None):
         """
         Realiza o download do arquivo mais recente. Recebe a janela de
         filtro em dias para reduzir carga na API. Utiliza 30 dias como padrão.
         """
-        id_newest_file = self._get_id_newest_file(lastdays_filter)
+        id_newest_file = self._get_id_newest_file(data_min, data_max)
 
         file_url = self.STA_URL + f"/arquivos/{id_newest_file}/conteudo"
         raw_file = requests.request("GET",
@@ -78,4 +89,4 @@ class BacenSTAHook(BaseHook):
             for chunk in raw_file.iter_content(chunk_size=512):
                 file.write(chunk)
 
-        print(f"Downloaded file com id {id_newest_file}.")
+        print(f"Downloaded file with id {id_newest_file}.")
