31b6b16833cf3e71f611cb107366694ba8b53f58
Nitai Bezerra da Silva <nitai.silva@planejamento.gov.br>
2021-02-20 09:46:31 -0300

    docs(copy_db_to_db_operator): improve docstring

--- plugins/operators/copy_db_to_db_operator.py
+++ plugins/operators/copy_db_to_db_operator.py
@@ -1,29 +1,19 @@
-
 """
-   2020
-   Deckers
-   Ministério da Economia / SEGES / CGINF
-   
-   Operador customizado piloto que copia tabela de um banco para outro entre MSSQL's para MSSQL's ou 
-   entre Postgres para Postgres ou entre MSSQL's para Postgres ou vice e versa.
-   
-    Carrega dado do Postgres/MSSQL para Postgres/MSSQL com psycopg2 e pyodbc
-    copiando todas as colunas e linhas já existentes na tabela de destino.
-    Tabela de destino deve ter a mesma estrutura e nome de tabela e colunas
-    que a tabela de origem, ou passar um select_sql que tenha as colunas de
-    destino.
-   
-   Args: 
-        destination_table (str): tabela de destino no formato schema.table
-        source_conn_id (str): connection origem do Airflow
-        source_provider (str): provider do banco origem (MSSQL ou PG)
-        destination_conn_id (str): connection destino do Airflow
-        destination_provider (str): provider do banco destino (MSSQL ou PG)
-        source_table (str): tabela de origem no formato schema.table
-        select_sql (str): query sql para consulta na origem. Se utilizado o
-        source_table será ignorado
+Operador que realiza a cópia de dados seguindo uma estratégia completa
+(full) entre de um banco de dados para outro. Os BDs podem ser Postgres
+ou SQL Server. Internamente são utilizadas as bibliotecas psycopg2 e
+pyodbc. Os dados copiados podem ser oriundos de uma tabela ou de um
+Select SQL.
 
-   
+Args:
+    destination_table (str): tabela de destino no formato schema.table
+    source_conn_id (str): connection origem do Airflow
+    source_provider (str): provider do banco origem ('MSSQL' ou 'PG')
+    destination_conn_id (str): connection destino do Airflow
+    destination_provider (str): provider do banco destino ('MSSQL' ou 'PG')
+    source_table (str): tabela de origem no formato schema.table
+    select_sql (str): query sql para consulta na origem. Se utilizado o
+    source_table será ignorado
 """
 
 from airflow.models.baseoperator import BaseOperator
@@ -56,7 +46,7 @@ class CopyDbToDbOperator(BaseOperator):
         self.select_sql = select_sql
         self.destination_truncate = destination_truncate
         self.chunksize = chunksize
-       
+
 
     def execute(self, context):
         copied = copy_db_to_db(
