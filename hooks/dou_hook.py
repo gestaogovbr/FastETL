68739975768d695010126df4b776158fdaf532e9
Nitai Bezerra da Silva <nitai.silva@economia.gov.br>
2021-06-21 18:07:39 -0300

    feat(dou_hook.py): create new hook to perform search on dou

---
+++ hooks/dou_hook.py
@@ -0,0 +1,62 @@
+"""
+Hook para realizar operações de consultas à API do Diário Oficial da União.
+"""
+
+import json
+import requests
+
+from airflow.utils.decorators import apply_defaults
+from airflow.hooks.base_hook import BaseHook
+
+from bs4 import BeautifulSoup
+
+class DOUHook(BaseHook):
+    IN_WEB_BASE_URL = 'https://www.in.gov.br/web/dou/-/'
+    IN_API_BASE_URL = 'https://www.in.gov.br/consulta/-/buscar/dou'
+
+    @apply_defaults
+    def __init__(self,
+                 *args,
+                 **kwargs):
+        pass
+
+    def search_text(self, search_term: str):
+        """
+        Search for a term in the API and return all ocurrences.
+
+        Args:
+            - search_term: The term to perform the search on.
+
+        Return:
+            - A list of dicts of structred results.
+        """
+
+        # Adiciona aspas duplas no inicio e no fim de cada termo para o
+        # caso de eles serem formados por mais de uma palavra
+        payload = {
+            'q': f'"{search_term}"',
+            's': 'do1',
+            'exactDate': 'dia',
+            'sortType': '0'
+        }
+        page = requests.get(self.IN_API_BASE_URL, params=payload, verify=False)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        script_tag = soup.find(
+            'script',
+            id='_br_com_seatecnologia_in_buscadou_BuscaDouPortlet_params'
+        )
+        search_results = json.loads(script_tag.contents[0])['jsonArray']
+        all_results = []
+        if search_results:
+            for content in search_results:
+                item = {}
+                item['section'] = 'Seção 1'
+                item['title'] = content['title']
+                item['href'] = self.IN_WEB_BASE_URL + content['urlTitle']
+                item['abstract'] = content['content']
+                item['date'] = content['pubDate']
+
+                all_results.append(item)
+
+        return all_results
