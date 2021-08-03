1c16797731920dfcc6be267ae720e1f46170988a
Nitai Bezerra da Silva <nitai.silva@economia.gov.br>
2021-08-03 13:47:41 -0300

    revert(encode_html.py): revert to previous file version
    it was raising these errors: `UnicodeEncodeError: 'ascii' codec can't encode characters in position
    2145-2146: ordinal not in range(128)` and `UnicodeEncodeError: 'ascii' codec can't encode character
    '\xc9' in position 2585: ordinal not in range(128)`

--- custom_functions/utils/encode_html.py
+++ custom_functions/utils/encode_html.py
@@ -1,15 +1,54 @@
-import html
-import unicodedata
-
 def replace_to_html_encode(text: str) -> str:
     "Replace accented characters with html entity equivalents."
+    html_map = (
+        ('À', '&Agrave;'),
+        ('Á', '&Aacute;'),
+        ('Â', '&Acirc;'),
+        ('Ã', '&Atilde;'),
+        ('Ç', '&Ccedil;'),
+        ('È', '&Egrave;'),
+        ('É', '&Eacute;'),
+        ('Ê', '&Ecirc;'),
+        ('Ì', '&Igrave;'),
+        ('Í', '&Iacute;'),
+        ('Î', '&Icirc;'),
+        ('Ñ', '&Ntilde;'),
+        ('Ò', '&Ograve;'),
+        ('Ó', '&Oacute;'),
+        ('Ô', '&Ocirc;'),
+        ('Õ', '&Otilde;'),
+        ('Ù', '&Ugrave;'),
+        ('Ú', '&Uacute;'),
+        ('Û', '&Ucirc;'),
+        ('à', '&agrave;'),
+        ('á', '&aacute;'),
+        ('â', '&acirc;'),
+        ('ã', '&atilde;'),
+        ('ç', '&ccedil;'),
+        ('è', '&egrave;'),
+        ('é', '&eacute;'),
+        ('ê', '&ecirc;'),
+        ('ì', '&igrave;'),
+        ('í', '&iacute;'),
+        ('î', '&icirc;'),
+        ('ñ', '&ntilde;'),
+        ('ò', '&ograve;'),
+        ('ó', '&oacute;'),
+        ('ô', '&ocirc;'),
+        ('õ', '&otilde;'),
+        ('ù', '&ugrave;'),
+        ('ú', '&uacute;'),
+        ('û', '&ucirc;'),
+        # (' ', '&#160;'),
+        ('§', '&#167;'),
+        ('ª', '&#170;'),
+        ('°', '&#176;'),
+        ('º', '&#186;'),
+        ('˚', '&#730;'),
+        ('"', '&#8220;')
+    )
 
-    SYMBOLS = '§ª°º˚"'
-
-    for entity, char in html.entities.html5.items():
-        if unicodedata.category(char[0]) == 'Ll' or char[0] in SYMBOLS:
-
-            text = text.replace(char, f'&{entity};')
+    for r in html_map:
+        text = text.replace(*r)
 
     return text
-
