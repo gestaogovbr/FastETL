b1f6347438ee6533dc844e452e08d772aa4fb4e0
Augusto Herrmann <augusto+github@herrmann.tech>
2021-08-13 15:39:20 -0300

    include warning before eventually deprecating replace_to_html_encode

--- custom_functions/utils/encode_html.py
+++ custom_functions/utils/encode_html.py
@@ -1,10 +1,16 @@
 import html
 import unicodedata
+import logging
 
 def replace_to_html_encode(text: str) -> str:
     "Replace accented characters with html entity equivalents."
 
     SYMBOLS = '§ª°º˚"'
+
+    logging.warn(
+        "If you are using Airflow's send_email, using replace_to_html_encode is"
+        ' discouraged. Use the mime_charset="utf-8" parameter instead.'
+    )
 
     for entity, char in html.entities.html5.items():
         if unicodedata.category(char[0]) in ('Ll', 'Lu') or char[0] in SYMBOLS:
