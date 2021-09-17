3bc88609ddb78e434bbc7b1cd0438247c580b278
Augusto Herrmann <augusto+github@herrmann.tech>
2021-09-17 16:15:26 -0300

    refactor(imarkdown_html): move function to existing file

--- custom_functions/utils/markdown_html.py
+++
@@ -1,9 +0,0 @@
-import inspect
-import textwrap
-
-from markdown import markdown
-
-def imarkdown_to_html(text: str) -> str:
-    """Transforms indented markdown text to html.
-    """
-    return markdown(inspect.cleandoc(textwrap.dedent(text)))
