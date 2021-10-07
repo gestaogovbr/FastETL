feff4df813bafd0032b4342715592ece0e421021
Nitai Bezerra da Silva <nitai.silva@economia.gov.br>
2021-10-07 12:11:27 -0300

    feat(db_to_db_hook_test): adds values to empty table example to force pandas to type columns
    also includes datetime column to improve test

--- db_to_db_hook_test.py
+++ db_to_db_hook_test.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 import pytest
 import pandas as pd
 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from plugins.FastETL.hooks.db_to_db_hook import DbToDbHook
+
 
 def insert_data(tablename, hook, data):
     """This script will populate database with initial data to run job"""
@@ -13,19 +15,33 @@ def insert_data(tablename, hook, data):
                        if_exists='replace',
                        index=False)
 
+
 def insert_initial_source_data(tablename, hook):
-    data = {'Name':['Tom', 'nick', 'krish', 'jack'],
-            'Age':[20, 21, 19, 18]}
-    insert_data(tablename, hook, data)
-def insert_initial_dest_empty_table(tablename, hook):
-    data = {'Name':[],
-            'Age':[]}
+    data = {'Name':['hendrix', 'nitai', 'krish', 'jesus'],
+            'Age':[27, 38, 1000, 33],
+            'Birth': [
+                datetime.strptime('1942-11-27', '%Y-%m-%d'),
+                datetime.strptime('1983-06-02', '%Y-%m-%d'),
+                datetime.strptime('3227-06-23', '%Y-%m-%d'),
+                datetime.strptime('0001-12-27', '%Y-%m-%d')]}
     insert_data(tablename, hook, data)
 
+
+def insert_initial_dest_empty_table(tablename, hook):
+    data = {'Name':['string'],
+            'Age':[1],
+            'Birth':[datetime.strptime('0001-12-27', '%Y-%m-%d')]}
+    insert_data(tablename, hook, data)
+
+
 def test_replicate_table_full():
-    oltp_hook = PostgresHook('oltp')
-    olap_hook = PostgresHook('olap')
-    insert_initial_source_data('example_table', oltp_hook)
-    insert_initial_dest_empty_table('example_table', olap_hook)
+    source_conn_id = 'oltp'
+    destination_conn_id = 'olap'
+    table_name = 'example_table'
+
+    oltp_hook = PostgresHook(source_conn_id)
+    olap_hook = PostgresHook(destination_conn_id)
+    insert_initial_source_data(table_name, oltp_hook)
+    insert_initial_dest_empty_table(table_name, olap_hook)
 
     assert True
