82019b8090c79ab3d04b7d0d624b5188244e1646
Nitai Bezerra da Silva <nitai.silva@economia.gov.br>
2021-10-06 21:28:52 -0300

    test(db_to_db_hook): loads initial data

--- db_to_db_hook_test.py
+++ db_to_db_hook_test.py
@@ -1,7 +1,24 @@
 import pytest
+import pandas as pd
 
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from plugins.FastETL.hooks.db_to_db_hook import DbToDbHook
 
+def insert_initial_data(tablename, hook):
+    """This script will populate database with initial data to run job"""
+    conn_engine = hook.get_sqlalchemy_engine()
+
+    data = {'Name':['Tom', 'nick', 'krish', 'jack'],
+            'Age':[20, 21, 19, 18]}
+    sample_data = pd.DataFrame(data)
+
+    sample_data.to_sql(name=tablename,
+                       con=conn_engine,
+                       if_exists='replace',
+                       index=False)
+
 def test_replicate_table_full():
-    # setup ambiente
+    oltp_hook = PostgresHook('oltp')
+    insert_initial_data('source_table', oltp_hook)
+
     assert True
