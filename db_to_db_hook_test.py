9d0cd2d60c1fda5459d4d2881959185f813043d3
Nitai Bezerra da Silva <nitai.silva@economia.gov.br>
2021-10-06 21:38:44 -0300

    test(db_to_db_hook): loads empty table on olap db

--- db_to_db_hook_test.py
+++ db_to_db_hook_test.py
@@ -4,21 +4,28 @@ import pandas as pd
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from plugins.FastETL.hooks.db_to_db_hook import DbToDbHook
 
-def insert_initial_data(tablename, hook):
+def insert_data(tablename, hook, data):
     """This script will populate database with initial data to run job"""
-    conn_engine = hook.get_sqlalchemy_engine()
-
-    data = {'Name':['Tom', 'nick', 'krish', 'jack'],
-            'Age':[20, 21, 19, 18]}
     sample_data = pd.DataFrame(data)
 
     sample_data.to_sql(name=tablename,
-                       con=conn_engine,
+                       con=hook.get_sqlalchemy_engine(),
                        if_exists='replace',
                        index=False)
 
+def insert_initial_source_data(tablename, hook):
+    data = {'Name':['Tom', 'nick', 'krish', 'jack'],
+            'Age':[20, 21, 19, 18]}
+    insert_data(tablename, hook, data)
+def insert_initial_dest_empty_table(tablename, hook):
+    data = {'Name':[],
+            'Age':[]}
+    insert_data(tablename, hook, data)
+
 def test_replicate_table_full():
     oltp_hook = PostgresHook('oltp')
-    insert_initial_data('source_table', oltp_hook)
+    olap_hook = PostgresHook('olap')
+    insert_initial_source_data('example_table', oltp_hook)
+    insert_initial_dest_empty_table('example_table', olap_hook)
 
     assert True
