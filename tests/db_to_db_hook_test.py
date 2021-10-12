04330c2cc2e9050ca4e96b5a6eaf46a2d3f5047b
Nitai Bezerra da Silva <nitai.silva@economia.gov.br>
2021-10-12 16:58:27 -0300

    test(db_to_db_hook): adds test for table replication over mssql dbs

--- tests/db_to_db_hook_test.py
+++ tests/db_to_db_hook_test.py
@@ -4,10 +4,12 @@ import pandas as pd
 from pandas._testing import assert_frame_equal
 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.odbc.hooks.odbc import OdbcHook
+
 from plugins.FastETL.hooks.db_to_db_hook import DbToDbHook
 
 
-def insert_data(tablename, hook, data):
+def _insert_data(tablename, hook, data):
     """This script will populate database with initial data to run job"""
     sample_data = pd.DataFrame(data)
 
@@ -17,33 +19,37 @@ def insert_data(tablename, hook, data):
                        index=False)
 
 
-def insert_initial_source_data(tablename, hook):
+def _date(date_: str) -> datetime:
+    return datetime.strptime(date_, '%Y-%m-%d').date()
+
+
+def _insert_initial_source_data(tablename, hook):
     data = {'Name':['hendrix', 'nitai', 'krish', 'jesus'],
             'Age':[27, 38, 1000, 33],
             'Birth': [
-                datetime.strptime('1942-11-27', '%Y-%m-%d'),
-                datetime.strptime('1983-06-02', '%Y-%m-%d'),
-                datetime.strptime('3227-06-23', '%Y-%m-%d'),
-                datetime.strptime('0001-12-27', '%Y-%m-%d')]}
-    insert_data(tablename, hook, data)
+                _date('1942-11-27'),
+                _date('1983-06-02'),
+                _date('3227-06-23'),
+                _date('0001-12-27')]}
+    _insert_data(tablename, hook, data)
 
 
-def insert_initial_dest_empty_table(tablename, hook):
+def _insert_initial_dest_empty_table(tablename, hook):
     data = {'Name':['string'],
             'Age':[1],
-            'Birth':[datetime.strptime('0001-12-27', '%Y-%m-%d')]}
-    insert_data(tablename, hook, data)
+            'Birth':[_date('0001-12-27')]}
+    _insert_data(tablename, hook, data)
 
 
-def test_replicate_table_full():
-    source_conn_id = 'pg-source'
-    dest_conn_id = 'pg-destination'
+def test_replicate_table_full_postgres():
+    source_conn_id = 'pg-source-conn'
+    dest_conn_id = 'pg-destination-conn'
     table_name = 'example_table'
 
     source_hook = PostgresHook(source_conn_id)
     dest_hook = PostgresHook(dest_conn_id)
-    insert_initial_source_data(table_name, source_hook)
-    insert_initial_dest_empty_table(table_name, dest_hook)
+    _insert_initial_source_data(table_name, source_hook)
+    _insert_initial_dest_empty_table(table_name, dest_hook)
 
     hook = DbToDbHook(
         source_conn_id=source_conn_id,
@@ -53,6 +59,31 @@ def test_replicate_table_full():
         ).full_copy(
         source_table=f'public.{table_name}',
         destination_table=f'public.{table_name}',
+        )
+
+    source_data = source_hook.get_pandas_df(f'select * from {table_name}')
+    dest_data = dest_hook.get_pandas_df(f'select * from {table_name}')
+
+    assert_frame_equal(source_data, dest_data)
+
+def test_replicate_table_full_mssql():
+    source_conn_id = 'mssql-source-conn'
+    dest_conn_id = 'mssql-destination-conn'
+    table_name = 'example_table'
+
+    source_hook = OdbcHook(source_conn_id)
+    dest_hook = OdbcHook(dest_conn_id)
+    _insert_initial_source_data(table_name, source_hook)
+    _insert_initial_dest_empty_table(table_name, dest_hook)
+
+    hook = DbToDbHook(
+        source_conn_id=source_conn_id,
+        destination_conn_id=dest_conn_id,
+        source_provider='MSSQL',
+        destination_provider='MSSQL'
+        ).full_copy(
+        source_table=f'dbo.{table_name}',
+        destination_table=f'dbo.{table_name}',
         )
 
     source_data = source_hook.get_pandas_df(f'select * from {table_name}')
