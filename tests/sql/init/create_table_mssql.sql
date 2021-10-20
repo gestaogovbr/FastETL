76e044e6c8b1319295b4d9ff752955ad7511df40
Nitai Bezerra da Silva <nitai.silva@economia.gov.br>
2021-10-19 21:36:42 -0300

    test(db_to_db_hook): adds support for large text data types on table schema replication
    fixes #41

--- tests/sql/init/create_table_mssql.sql
+++ tests/sql/init/create_table_mssql.sql
@@ -1,9 +1,11 @@
 CREATE TABLE {table_name} (
-	Name      VARCHAR(MAX) COLLATE SQL_Latin1_General_CP1_CI_AS,
-	Age       BIGINT,
-	Weight    FLOAT,
-	Birth     DATE,
-	Active    BIT,
-	date_time DATETIME2
+	Name          VARCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS,
+	Description   TEXT COLLATE SQL_Latin1_General_CP1_CI_AS,
+	Description2  VARCHAR(MAX) COLLATE SQL_Latin1_General_CP1_CI_AS,
+	Age           BIGINT,
+	Weight        FLOAT,
+	Birth         DATE,
+	Active        BIT,
+	date_time     DATETIME2
 );
 
--- tests/sql/init/create_table_pg.sql
+++ tests/sql/init/create_table_pg.sql
@@ -1,8 +1,10 @@
 CREATE TABLE {table_name} (
-    "Name"       TEXT,
-	"Age"        INTEGER,
-	"Weight"     FLOAT,
-	"Birth"      DATE,
-	"Active"     BOOL,
-    "date_time"  TIMESTAMP
+    "Name"         VARCHAR(10),
+    "Description"  TEXT,
+    "Description2" VARCHAR,
+	"Age"          INTEGER,
+	"Weight"       FLOAT,
+	"Birth"        DATE,
+	"Active"       BOOL,
+    "date_time"    TIMESTAMP
 );
