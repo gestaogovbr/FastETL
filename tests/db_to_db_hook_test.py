6b63dd5f5a6b5798aff8ddda8fa0fd1054153aaf
Nitai Bezerra da Silva <nitai.silva@economia.gov.br>
2021-10-20 07:10:42 -0300

    refactor(db_to_db_hook_test): removes unnecessary code

--- tests/db_to_db_hook_test.py
+++ tests/db_to_db_hook_test.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time
+from datetime import datetime, date, time
 import logging
 import pytest
 
@@ -30,10 +30,6 @@ def _create_initial_table(table_name: str, hook: DbApiHook,
     hook.run(sql_stmt.format(table_name=table_name))
 
 
-def _datetime(date_: str) -> datetime:
-    return datetime.strptime(f'{date_} 00:00:01', '%Y-%m-%d %H:%M:%S')
-
-
 def _insert_initial_source_table_n_data(table_name: str, hook: DbApiHook,
                                         db_provider: str) -> None:
     _create_initial_table(table_name, hook, db_provider)
@@ -51,16 +47,16 @@ def _insert_initial_source_table_n_data(table_name: str, hook: DbApiHook,
             'Age':[27, 38, 1000, 33],
             'Weight':[1000.0111111111111, 75.33, 333.33, 12345.54320091],
             'Birth':[
-                _datetime('1942-11-27').date(),
-                _datetime('1983-06-02').date(),
-                _datetime('3227-06-23').date(),
-                _datetime('0001-12-27').date()],
+                date(1942, 11, 27),
+                date(1983, 6, 2),
+                date(3227, 6, 23),
+                date(1, 12, 27)],
             'Active':[False, True, True, True],
             'date_time':[
-                _datetime('1942-11-27'),
-                _datetime('1983-06-02'),
-                _datetime('3227-06-23'),
-                _datetime('0001-12-27')],
+                datetime(1942, 11, 27, 1, 2, 3),
+                datetime(1983, 6, 2, 1, 2, 3),
+                datetime(3227, 6, 23, 1, 2, 3),
+                datetime(1, 12, 27, 1, 2, 3)],
             }
 
     pd.DataFrame(data).to_sql(name=table_name,
