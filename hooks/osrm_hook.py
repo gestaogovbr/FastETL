b52b2fa82c23686a59de125b931e6e2f55b7bef4
Augusto Herrmann <augusto+github@herrmann.tech>
2022-05-18 16:54:10 -0300

    Complete the docstrings for OSRMHook

--- hooks/osrm_hook.py
+++ hooks/osrm_hook.py
@@ -1,5 +1,5 @@
-"""Airflow hooks to access the OSRM API to calculate routes and
-distances.
+"""Airflow hooks to access the Open Street Routing Machine (OSRM) API to
+calculate routes and distances.
 """
 import urllib
 import requests
@@ -13,6 +13,19 @@ from FastETL.custom_functions.config import USER_AGENT
 
 class OSRMHook(BaseHook):
     """Provides access to the Open Street Routing Machine (OSRM) API.
+
+    Args:
+        conn_id (str): The Airflow connection id to use.
+
+    Attributes:
+        api_endpoint (str): URL of the OSRM API endpoint.
+
+    Examples:
+        Instantiate the class by passing the Airflow connection id.
+
+        >>> hook = OSRMHook(conn_id='osrm_api')
+        >>> isinstance(hook, OSRMHook)
+        True
     """
 
     @apply_defaults
@@ -25,7 +38,7 @@ class OSRMHook(BaseHook):
         self.conn_id = conn_id
 
     @cached_property
-    def api_endpoint(self):
+    def api_endpoint(self) -> str:
         """Gets the API endpoint from the Airflow connection.
         """
         conn = BaseHook.get_connection(self.conn_id)
@@ -40,6 +53,21 @@ class OSRMHook(BaseHook):
         profile: str = 'driving') -> dict:
         """Gets the calculated routes from the OSRM API from the defined
         origin and destination point, using the specified profile.
+
+        Args:
+            origin (Tuple[float, float]): The point of origin for the
+                route, as a tuple of latitude, longitude.
+            destination (Tuple[float, float]): The point of destination
+                for the route, as a tuple of latitude, longitude.
+            profile (:obj:`str`, optional): The profile for calculating
+                the route (e.g.) 'bike' or 'foot'. Defaults to 'driving'.
+
+        Returns:
+            dict: A `dict` with the data returned from the call to the
+                OSRM API.
+
+        Raises:
+            ValueError: If the code returned by the API is not 200.
         """
         lat_o, long_o = origin
         lat_d, long_d = destination
@@ -62,6 +90,13 @@ class OSRMHook(BaseHook):
     @staticmethod
     def get_shortest_distance(data: dict) -> float:
         """Gets the distance of the shortest route using the OSRM API.
+
+        Args:
+            data (dict): The route data returned by `get_route`.
+
+        Returns:
+            float: The distance, in kilometers, of the first (shortest)
+                route.
         """
         if data['code'] == 'Ok':
             return data['routes'][0]['distance'] / 1000.0
