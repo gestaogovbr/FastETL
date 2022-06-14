6d911448dd50ad044742b4d01b9e5460db072b7b
Augusto Herrmann <augusto+github@herrmann.tech>
2022-06-14 12:02:54 -0300

    Corrige atualização do banco
    usando commit da transação e chunksize.
    
    Fixes #72 #73

--- operators/osrm_distance_operator.py
+++ operators/osrm_distance_operator.py
@@ -5,11 +5,7 @@ from functools import cached_property
 from collections.abc import Iterable
 from typing import Union, Any, Tuple
 
-import pyodbc
-
 from airflow.hooks.base import BaseHook
-from airflow.hooks.mssql_hook import MsSqlHook
-from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models.connection import Connection
 
 from airflow.models.baseoperator import BaseOperator
@@ -41,6 +37,7 @@ class OSRMDistanceDbOperator(BaseOperator):
         distance_column (str): Name of the column that will be written
             to, with the calculated shortest route distance, in
             kilometers.
+        chunksize (int): Size of the chunk read from the database.
     """
     ui_color = '#90d572'
 
@@ -54,6 +51,7 @@ class OSRMDistanceDbOperator(BaseOperator):
         origin_columns: Union[Tuple[str, str], str],
         destination_columns: Union[Tuple[str, str], str],
         distance_column: str,
+        chunksize: int = 100,
         **kwargs):
         super().__init__(**kwargs)
         self.db_conn_id = db_conn_id
@@ -64,7 +62,9 @@ class OSRMDistanceDbOperator(BaseOperator):
         self.origin_columns = origin_columns
         self.destination_columns = destination_columns
         self.distance_column = distance_column
+        self.chunksize = chunksize
         self.conn_type: Connection = None
+        self.osrm_hook: BaseHook = None
         if not (
             (isinstance(origin_columns, Iterable) and len(origin_columns) == 2)
             or isinstance(origin_columns, str)):
@@ -91,19 +91,6 @@ class OSRMDistanceDbOperator(BaseOperator):
         """The connection type for the database."""
         return self.airflow_db_conn.conn_type
 
-    @cached_property
-    def db_hook(self) -> BaseHook:
-        """The database hook for the operator."""
-        db_hook: BaseHook = None
-        if self.conn_type == 'mssql':
-            db_hook = MsSqlHook(mssql_conn_id=self.db_conn_id)
-        elif self.conn_type == 'postgres':
-            db_hook = PostgresHook(postgres_conn_id=self.db_conn_id)
-        else:
-            raise NotImplementedError(
-                f'Support for {self.conn_type} database is not implemented.')
-        return db_hook
-
     @property
     def select_sql(self) -> str:
         """Create the SELECT SQL query for the points of origin and
@@ -113,6 +100,7 @@ class OSRMDistanceDbOperator(BaseOperator):
             str: The SQL query for the SELECT.
         """
         query = 'SELECT '
+        where = 'WHERE '
 
         # add the primary key columns
         query += ', '.join(self.pk_columns) + ', '
@@ -123,11 +111,14 @@ class OSRMDistanceDbOperator(BaseOperator):
                 f'{self.origin_columns}.Lat AS origin_latitude, '
                 f'{self.origin_columns}.Long AS origin_longitude, '
             )
+            where += f'{self.origin_columns} IS NOT NULL '
         else: # latitude and longitude float columns
             query += (
                 f'{self.origin_columns[0]} AS origin_latitude, '
                 f'{self.origin_columns[1]} AS origin_longitude, '
             )
+            where += ' AND '.join(
+                (f'{column} IS NOT NULL ' for column in self.origin_columns))
 
         # add the destination coordinate columns
         if isinstance(self.destination_columns, str): # geometry type column
@@ -135,13 +126,18 @@ class OSRMDistanceDbOperator(BaseOperator):
                 f'{self.destination_columns}.Lat AS destination_latitude, '
                 f'{self.destination_columns}.Long AS destination_longitude '
             )
+            where += f'AND {self.destination_columns} IS NOT NULL;'
         else: # latitude and longitude float columns
             query += (
                 f'{self.destination_columns[0]} AS destination_latitude, '
                 f'{self.destination_columns[1]} AS destination_longitude '
             )
+            where += 'AND ' + ' AND '.join((
+                f'{column} IS NOT NULL '
+                for column in self.destination_columns))
 
-        query += f'FROM {self.table_scheme}.{self.table_name};'
+        query += f'FROM {self.table_scheme}.{self.table_name} '
+        query += where + ';'
 
         return query
 
@@ -175,20 +171,41 @@ class OSRMDistanceDbOperator(BaseOperator):
 
         return query
 
+    def _update_row(self, row: list) -> str:
+        """Get the value for the distance for a given row.
+
+        Args:
+            row (list): The database row returned by the SELECT query.
+
+        Returns:
+            str: The UPDATE query for this particular row.
+        """
+        origin_latitude, origin_longitude = row[-4: -2]
+        destination_latitude, destination_longitude = row[-2:]
+        primary_keys = row[:len(self.pk_columns)]
+        return self.update_sql(
+            primary_keys,
+            get_shortest_distance(
+                self.osrm_hook.get_route(
+                    origin=(origin_latitude, origin_longitude),
+                    destination=(destination_latitude, destination_longitude)
+                )
+            )
+        )
+
     def execute(self, context: dict):
         """Execute the operator."""
-        with DbConnection(self.db_conn_id,
-                        provider=self.db_conn_type.upper()) as db_conn:
-            with db_conn.cursor() as db_cursor:
-                osrm_hook = OSRMHook(self.osrm_conn_id)
-                for row in db_cursor.execute(self.select_sql).fetchall():
-                    origin_latitude, origin_longitude = row[-4: -2]
-                    destination_latitude, destination_longitude = row[-2:]
-                    primary_keys = row[:len(self.pk_columns)]
-                    distance = get_shortest_distance(
-                        osrm_hook.get_route(
-                            origin=(origin_latitude, origin_longitude),
-                            destination=(destination_latitude, destination_longitude)
-                        )
-                    )
-                    db_cursor.execute(self.update_sql(primary_keys, distance))
+        self.osrm_hook = OSRMHook(self.osrm_conn_id)
+        with DbConnection(
+                conn_id=self.db_conn_id,
+                provider=self.db_conn_type.upper()) as read_db_conn:
+            with DbConnection(
+                    conn_id=self.db_conn_id,
+                    provider=self.db_conn_type.upper()) as write_db_conn:
+                with read_db_conn.cursor() as select_cursor:
+                    select_cursor.execute(self.select_sql)
+                    while rows := select_cursor.fetchmany(self.chunksize):
+                        for row in rows:
+                            with write_db_conn.cursor() as update_cursor:
+                                update_cursor.execute(self._update_row(row))
+                                update_cursor.commit()
