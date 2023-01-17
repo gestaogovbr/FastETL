383142c2dc4ab006a95cac460090b3a3b35af8c4
Vitor Bellini <vitor.bellini@economia.gov.br>
2023-01-17 17:07:47 -0300

    [get_page_source] add remove folder

--- ro_dou_functions/get_page_source.py
+++ ro_dou_functions/get_page_source.py
@@ -1,17 +1,21 @@
+import shutil
 from flask import Flask, request, json
+from selenium import webdriver
 
 app = Flask(__name__)
 
 @app.route('/get_page_source', methods=["POST"])
 def get_page_source():
-    from selenium import webdriver
-
     data = json.loads(request.data)
     url = data["dou_url"]
     driver = webdriver.Chrome()
     driver.get(url)
     result = driver.page_source
+    driver.close()
     driver.quit()
+
+    tmp_folder = driver.capabilities['chrome']['userDataDir']
+    shutil.rmtree(tmp_folder, ignore_errors=True)
 
     return result
 
