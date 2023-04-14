ab596c8af02d659a247c83e14f037b9121596e74
Augusto Herrmann <augusto+github@herrmann.tech>
2023-04-13 23:13:31 -0300

    Fix frictionless version dependency constraint

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "alembic>=1.8.1",
         "beautifulsoup4>=4.1.11",
         "ckanapi>=4.6",
-        "frictionless>5.11.1",
+        "frictionless>=5.11.1",
         "markdown>=3.4.1",
         "odfpy>=1.4.1",
         "pandas>=1.5.2,<2",
