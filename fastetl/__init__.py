e2f3a72842042ae469e86513c78bf478be4aaa72
Vitor <vitor.bellini@economia.gov.br>
2023-04-14 17:28:14 -0300

    add if __init__ load function

--- fastetl/__init__.py
+++ fastetl/__init__.py
@@ -1,15 +1,17 @@
 from importlib.metadata import version
+import os
 
-__name__ = "apache-airflow-providers-fastetl"
-__version__ = version(__name__)
+if not os.getenv('FASTETL_DEV'):
+    __name__ = "apache-airflow-providers-fastetl"
+    __version__ = version(__name__)
 
-# This is needed to allow Airflow to pick up specific metadata fields
-# it needs for certain features.
+    # This is needed to allow Airflow to pick up specific metadata fields
+    # it needs for certain features.
 
-def get_provider_info():
-    return {
-        "package-name": __name__,
-        "name": "FastETL Apache Airflow Provider",
-        "description": "Copy data between Db Tables.",
-        "versions": [__version__],
-    }
+    def get_provider_info():
+        return {
+            "package-name": __name__,
+            "name": "FastETL Apache Airflow Provider",
+            "description": "Copy data between Db Tables.",
+            "versions": [__version__],
+        }
