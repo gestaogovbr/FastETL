8e9bc59ebcfc9db1200cf29595e30eb3436e5320
Vitor <vitor.bellini@economia.gov.br>
2023-06-16 19:53:02 -0300

    update package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 
 """Perform the package apache-airflow-providers-fastetl setup."""
 setup(
