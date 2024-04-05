544ac12eb8bd612ce2f1b67ae55f86f4938d3280
Augusto Herrmann <augusto+github@herrmann.tech>
2024-04-05 11:30:42 -0300

    Disable pylint warning on long lines of lorem ipsum

--- tests/test_db_to_db_operator.py
+++ tests/test_db_to_db_operator.py
@@ -33,6 +33,7 @@ def _create_initial_table(table_name: str, hook: DbApiHook, db_provider: str) ->
 
 NAMES = ["hendrix", "nitai", "krishna", "jesus", "Danielle", "Augusto"]
 DESCRIPTIONS = [
+    # pylint: disable=line-too-long
     "Eh um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página quando estiver examinando sua diagramação. A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de Conteúdo aqui, conteúdo aqui, fazendo com que ele tenha uma aparência similar a de um texto legível.",
     "Muitos softwares de publicação e editores de páginas na internet agora usam Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em sua fase de construção. Várias versões novas surgiram ao longo dos anos, eventualmente por acidente, e às vezes de propósito",
     "Existem muitas variações disponíveis de passagens de Lorem Ipsum, mas a maioria sofreu algum tipo de alteração, seja por inserção de passagens com humor, ou palavras aleatórias que não parecem nem um pouco convincentes. Se você pretende usar uma passagem de Lorem Ipsum, precisa ter certeza de que não há algo embaraçoso escrito ",
