75048c2001f575e3369d51b25008646a55ab8ce5
Eduardo Lauer <edulauer@hotmail.com>
2024-04-29 18:06:32 -0300

    code tests

--- tests/test_db_connection.py
+++ tests/test_db_connection.py
@@ -3,159 +3,72 @@
 from datetime import date, datetime
 import logging
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine, URL
+import airflow.providers.postgres.hooks.postgres as postgres
+import airflow.providers.microsoft.mssql.hooks.mssql as mssql
 from airflow.providers.common.sql.hooks.sql import DbApiHook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.odbc.hooks.odbc import OdbcHook
-from fastetl.custom_functions.utils.db_connection import get_mssql_odbc_engine
+from fastetl.custom_functions.utils.db_connection import (
+    get_mssql_odbc_engine,
+    get_hook_and_engine_by_provider,
+    get_conn_type,
+)
 
 
 # Tests
+@pytest.mark.parametrize(
+    "conn_id", "engine_str",
+    [(
+        "mssql-source-conn",
+        "Engine(mssql+pyodbc://?odbc_connect=Driver%3D%7BODBC+Driver+17+for+SQL+Server%7D%3BServer%3Dmssql-source%2C+1433%3B+++++++++++++++++++++Database%3Dmaster%3BUid%3Dsa%3BPwd%3DozoBaroF2021%3B",
+    )],
+)
+def test_get_mssql_odbc_engine(conn_id: str, engine_str: str):
+    engine = get_mssql_odbc_engine(conn_id)
+    assert (str(engine) == engine_str)
 
-def test_get_mssql_odbc_engine(
-    conn_id: str
-):
-    conn_id = "mssql-source-conn"
-    connection_url = get_mssql_odbc_engine(conn_id)
 
-    assert connection_url == "mssql+pyodbc:"
+@pytest.mark.parametrize(
+        "conn_id, expected_conn_type",
+        [
+            ("mssql-source-conn", "mssql"),
+            ("postgres-source-conn", "postgres")
+        ]
+    )
+def test_get_conn_type(conn_id: str, expected_conn_type: str):
+    conn_type = get_conn_type(conn_id)
+    assert conn_type == expected_conn_type
+
 
 @pytest.mark.parametrize(
     "conn_id",
     [
-        "postgres",
-        "mssql",
-        "mysql"
-    ]
-)
-def test_get_hook_and_engine_by_provider(
-    conn_id: str
-):
-    pass
-
-@pytest.mark.parametrize(
-    "conn_id",
-    [
-        "postgres",
-        "mssql",
-        "mysql"
-    ]
-)
-def test_get_get_conn_type(
-    conn_id: str
-):
-    pass
-
-def test_db_as_hook(
-    conn_id: str
-):
-    pass
-
-def test_db_as_engine(
-    conn_id: str
-):
-    pass
-
-
-
-@pytest.mark.parametrize(
-    "source_conn_id, source_hook_cls, source_provider, dest_conn_id, "
-    "dest_hook_cls, destination_provider, has_dest_table",
-    [
-        (
-            "postgres-source-conn",
-            PostgresHook,
-            "postgres",
-            "postgres-destination-conn",
-            PostgresHook,
-            "postgres",
-            True,
-        ),
-        (
-            "mssql-source-conn",
-            OdbcHook,
-            "mssql",
-            "mssql-destination-conn",
-            OdbcHook,
-            "mssql",
-            True,
-        ),
-        (
-            "postgres-source-conn",
-            PostgresHook,
-            "postgres",
-            "mssql-destination-conn",
-            OdbcHook,
-            "mssql",
-            True,
-        ),
-        (
-            "mssql-source-conn",
-            OdbcHook,
-            "mssql",
-            "postgres-destination-conn",
-            PostgresHook,
-            "postgres",
-            True,
-        ),
-        (
-            "postgres-source-conn",
-            PostgresHook,
-            "postgres",
-            "postgres-destination-conn",
-            PostgresHook,
-            "postgres",
-            False,
-        ),
-        (
-            "mssql-source-conn",
-            OdbcHook,
-            "mssql",
-            "mssql-destination-conn",
-            OdbcHook,
-            "mssql",
-            False,
-        ),
-        (
-            "postgres-source-conn",
-            PostgresHook,
-            "postgres",
-            "mssql-destination-conn",
-            OdbcHook,
-            "mssql",
-            False,
-        ),
-        (
-            "mssql-source-conn",
-            OdbcHook,
-            "mssql",
-            "postgres-destination-conn",
-            PostgresHook,
-            "postgres",
-            False,
-        ),
+        "mssql-source-conn",
+        "postgres-source-conn",
     ],
 )
-def test_full_table_replication_various_db_types(
-    source_conn_id: str,
-    source_hook_cls: DbApiHook,
-    source_provider: str,
-    dest_conn_id: str,
-    dest_hook_cls: DbApiHook,
-    destination_provider: str,
-    has_dest_table: bool,
-):
-    """Test full table replication using various database types.
-
-    Args:
-        source_conn_id (str): source database connection id.
-        source_hook_cls (DbApiHook): source database hook class.
-        source_provider (str): source database provider.
-        dest_conn_id (str): destination database connection id.
-        dest_hook_cls (DbApiHook): destination database hook class.
-        destination_provider (str): destination database provider.
-        has_dest_table (bool): whether or not to create the table at
-            the destination database before testing replication.
-    """
+def test_get_hook_and_engine_by_provider(conn_id: str):
+    hook, engine = get_hook_and_engine_by_provider(conn_id)
+    if get_conn_type(conn_id) == "postgres":
+        assert isinstance(hook, postgres.PostgresHook)
+        assert isinstance(
+            engine, create_engine("postgresql://root:***@postgres-source/db").__class__
+        )
+    elif get_conn_type(conn_id) == "mssql":
+        assert isinstance(hook, mssql.MsSqlHook)
+        assert isinstance(
+            engine,
+            create_engine(
+                "mssql+pyodbc://?odbc_connect=Driver%3D%7BODBC+Driver+17+for+SQL+Server%7D%3BServer%3Dmssql-source%2C+1433%3B+++++++++++++++++++++Database%3Dmaster%3BUid%3Dsa%3BPwd%3DozoBaroF2021%3B"
+            ).__class__,
+        )
 
 
-    assert_frame_equal(source_data, dest_data)
+def test_db_as_hook(conn_id: str):
+    pass
+
+
+def test_db_as_engine(conn_id: str):
+    pass
