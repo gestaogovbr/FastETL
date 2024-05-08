943db1e2af3fc808b93ba904a94fd714e7a6a536
Augusto Herrmann <augusto+github@herrmann.tech>
2024-05-08 11:49:55 -0300

    Add docstrings and divide long strings into multiple lines
    to solve pylint warnings

--- tests/test_db_connection.py
+++ tests/test_db_connection.py
@@ -1,5 +1,7 @@
 """Tests for the DbConnection and Engine functions."""
 
+from typing import Literal
+
 import pytest
 import psycopg2
 import pyodbc
@@ -23,12 +25,22 @@ from fastetl.custom_functions.utils.db_connection import (
     [
         (
             "mssql-source-conn",
-            "Engine(mssql+pyodbc://?odbc_connect=Driver%3D%7BODBC+Driver+17+for+SQL+Server%7D%3BServer%3D"
-            + "mssql-source%2C+1433%3B+++++++++++++++++++++Database%3Dmaster%3BUid%3Dsa%3BPwd%3DozoBaroF2021%3B)",
+            "Engine(mssql+pyodbc://?odbc_connect=Driver%3D%7BODBC+Driver+17+for+SQL+Server%7D%3B"
+            "Server%3Dmssql-source%2C+1433%3B+++++++++++++++++++++"
+            "Database%3Dmaster%3BUid%3Dsa%3BPwd%3DozoBaroF2021%3B)",
         ),
     ],
 )
 def test_get_mssql_odbc_engine(conn_id: str, engine_str: str):
+    """Test that the
+    fastetl.custom_functions.utils.db_connection.get_mssql_odbc_engine
+    function returns the correct engine for MS SQL Server, by checking
+    the engine representation as a string.
+
+    Args:
+        conn_id (str): The connection id.
+        engine_str (str): The expected engine string.
+    """
     engine = get_mssql_odbc_engine(conn_id)
     assert str(engine) == engine_str
 
@@ -38,6 +50,13 @@ def test_get_mssql_odbc_engine(conn_id: str, engine_str: str):
     [("mssql-source-conn", "mssql"), ("postgres-source-conn", "postgres")],
 )
 def test_get_conn_type(conn_id: str, expected_conn_type: str):
+    """Test that the connection type is as expected for the given
+    connections.
+
+    Args:
+        conn_id (str): The connection id.
+        expected_conn_type (str): The expected connection type.
+    """
     conn_type = get_conn_type(conn_id)
     assert conn_type == expected_conn_type
 
@@ -50,6 +69,11 @@ def test_get_conn_type(conn_id: str, expected_conn_type: str):
     ],
 )
 def test_get_hook_and_engine_by_provider(conn_id: str):
+    """Test that the hook and engine returned are the expected ones for
+    the given connections.
+    Args:
+        conn_id (str): The connection id.
+    """
 
     hook, engine = get_hook_and_engine_by_provider(conn_id)
 
@@ -59,10 +83,10 @@ def test_get_hook_and_engine_by_provider(conn_id: str):
 
     elif get_conn_type(conn_id) == "mssql":
         assert isinstance(hook, MsSqlHook)
-        assert (
-            str(engine.url)
-            == "mssql+pyodbc://?odbc_connect=Driver%3D%7BODBC+Driver+17+for+SQL+Server%7D%3BServer%3D"
-            + "mssql-source%2C+1433%3B+++++++++++++++++++++Database%3Dmaster%3BUid%3Dsa%3BPwd%3DozoBaroF2021%3B"
+        assert str(engine.url) == (
+            "mssql+pyodbc://?odbc_connect=Driver%3D%7BODBC+Driver+17+for+SQL+Server%7D%3B"
+            "Server%3Dmssql-source%2C+1433%3B+++++++++++++++++++++"
+            "Database%3Dmaster%3BUid%3Dsa%3BPwd%3DozoBaroF2021%3B"
         )
 
 
@@ -77,8 +101,14 @@ def test_get_hook_and_engine_by_provider(conn_id: str):
         ("postgres-source-conn", "engine"),
     ],
 )
-def test_db_connection(conn_id: str, use: str):
+def test_db_connection(conn_id: str, use: Literal["hook", "connection", "engine"]):
+    """Test that the DbConnection is as expected for the given connections.
 
+    Args:
+        conn_id (str): The connection id.
+        use (Literal["hook", "connection", "engine"]): What object to test.
+            Either one of the following: "hook", "connection", or "engine".
+    """
     with DbConnection(conn_id=conn_id, use=use) as db_hook:
         conn_type = get_conn_type(conn_id)
         if conn_type == "postgres":
@@ -93,6 +123,9 @@ def test_db_connection(conn_id: str, use: str):
                 isinstance(db_hook, MsSqlHook)
                 or isinstance(db_hook, pyodbc.Connection)
                 or str(db_hook)
-                == "Engine(mssql+pyodbc://?odbc_connect=Driver%3D%7BODBC+Driver+17+for+SQL+Server%7D%3BServer%3D"
-                + "mssql-source%2C+1433%3B+++++++++++++++++++++Database%3Dmaster%3BUid%3Dsa%3BPwd%3DozoBaroF2021%3B)"
+                == (
+                    "Engine(mssql+pyodbc://?odbc_connect=Driver%3D%7BODBC+Driver+17+for+SQL+Server%7D%3B"
+                    "Server%3Dmssql-source%2C+1433%3B+++++++++++++++++++++"
+                    "Database%3Dmaster%3BUid%3Dsa%3BPwd%3DozoBaroF2021%3B)"
+                )
             )
