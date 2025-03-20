8cd833f20fb125ccfbfec2beb27a01968d28b64a
Eduardo Lauer <edulauer@hotmail.com>
2025-03-20 17:35:21 -0300

    create tasks for query replication

--- tests/test_dag.py
+++ tests/test_dag.py
@@ -32,3 +32,16 @@ with DAG(
                     "table": 'destination_table',
                 },
             )
+        for dest_conf in db_confs:
+            DbToDbOperator(
+                task_id=f'test_from_{source_conf[0]}_query_to_{dest_conf[0]}',
+                source={
+                    "conn_id": f'{source_conf[0]}-source-conn',
+                    "query": f"SELECT * FROM {source_conf[1]}.source_table"
+                },
+                destination={
+                    "conn_id": f'{dest_conf[0]}-destination-conn',
+                    "schema": dest_conf[1],
+                    "table": 'destination_table',
+                },
+            )
