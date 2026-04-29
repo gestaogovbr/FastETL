9fb5d596cd8c3d4d0efcf6cdd1ca0c7b5e24afc1
salomaolopes <salopesousa@hotmail.com>
2026-04-29 08:45:19 -0300

    fix: update sharepoint imports in __init__.py after refactor

--- fastetl/custom_functions/__init__.py
+++ fastetl/custom_functions/__init__.py
@@ -1,7 +1,6 @@
-# fastetl/custom_functions/__init__.py
 from fastetl.custom_functions.sharepoint import (
     get_sharepoint_client,
+    list_sharepoint_files,
     upload_to_sharepoint,
     download_from_sharepoint,
-    listar_arquivos_sharepoint,
 )
